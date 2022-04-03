1a22beb5d6d31e608aa2e66e980a13b274a50746
Sean Gau <rrtw0627@gmail.com>
2022-04-03 19:04:36 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+const gulp = require('gulp');
+const pug = require('gulp-pug');
+const sass = require('gulp-sass')(require('sass'));
+const data = require('gulp-data');
+const connect = require('gulp-connect');
+const fs = require('fs');
+
+function build() {  
+  let build_time = new Date().getTime();
+  gulp.src('src/sass/**/*.sass')
+  .pipe(data((file) => {
+    console.log("[build] "+file['history']);
+  }))
+  .pipe(sass.sync().on('error', sass.logError))
+  .pipe(gulp.dest('./static/assets/css'));
+
+  gulp.src('src/pug/**/index.pug')
+  .pipe(data((file) => {
+    console.log("[build] "+file['history']);
+    const result = {
+      timestamp: build_time
+    };
+    return result;
+  }))
+  .pipe(pug())
+  .pipe(gulp.dest('./static/'))
+  .pipe(connect.reload());
+}
+
+gulp.task('server', function () {
+    connect.server({
+        port: 3000,
+        livereload: true,
+        root: 'static'
+    })
+    build();
+
+    gulp.watch(['src/**/*.pug', 'src/**/*.sass', 'static/assets/js/*.js', 'data/*.json'], function(event){
+      build();
+      event();
+    });
+});
