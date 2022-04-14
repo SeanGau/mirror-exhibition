46a4c3e815f5cd33201dc731af2e482087e4d29e
Sean Gau <rrtw0627@gmail.com>
2022-04-14 19:40:31 +0800

    enhance gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,41 +5,49 @@ const data = require('gulp-data');
 const connect = require('gulp-connect');
 const fs = require('fs');
 
-function build() {  
+function buildSass() {
   let build_time = new Date().getTime();
   gulp.src('src/sass/**/*.sass')
-  .pipe(data((file) => {
-    console.log("[build] "+file['history']);
-  }))
-  .pipe(sass.sync().on('error', sass.logError))
-  .pipe(gulp.dest('./static/assets/css'));
+    .pipe(data((file) => {
+      console.log("[build] " + file['history']);
+    }))
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./static/assets/css'))
+    .pipe(connect.reload());
+}
 
-  gulp.src(['src/pug/**/*.pug','!src/pug/**/_*.pug'])
-  .pipe(data((file) => {
-    console.log("[build] "+file['history']);
-    const result = {
-      timestamp: build_time
-    };
-    return result;
-  }))
-  .pipe(pug({
-    basedir: "src/pug/"
-  }))
-  .pipe(gulp.dest('./static/'))
-  .pipe(connect.reload());
+function buildPug() {
+  let build_time = new Date().getTime();
+  gulp.src(['src/pug/**/*.pug', '!src/pug/**/_*.pug'])
+    .pipe(data((file) => {
+      console.log("[build] " + file['history']);
+      const result = {
+        timestamp: build_time
+      };
+      return result;
+    }))
+    .pipe(pug({
+      basedir: "src/pug/"
+    }))
+    .pipe(gulp.dest('./static/'))
+    .pipe(connect.reload());
 }
 
 gulp.task('server', function () {
-    connect.server({
-        port: 3000,
-        livereload: true,
-        host: '::',
-        root: 'static'
-    })
-    build();
-
-    gulp.watch(['src/**/*.pug', 'src/**/*.sass', 'static/assets/js/*.js', 'data/*.json'], function(event){
-      build();
-      event();
-    });
+  connect.server({
+    port: 3000,
+    livereload: true,
+    host: '::',
+    root: 'static'
+  })
+  buildSass();
+  buildPug();
+  const watchSass = gulp.watch(['src/**/*.sass', 'data/*.json'], function (cb) {
+    buildSass();
+    cb();
+  });
+  const watchPug = gulp.watch(['src/**/*.pug', 'static/assets/js/*.js', 'data/*.json'], function (cb) {
+    buildPug();
+    cb();
+  });
 });
